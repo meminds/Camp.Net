9abb8721c9730412768d8a3e3ec3c01068591a96
Muhammet Bas <190201113@uzem.education>
2022-02-16 21:42:43 +0300

    11 Şubat

---
+++ Maths/FourOperations.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maths
+{
+    internal class FourOperations
+    {
+        public int Sum(int number1, int number2)
+        {
+            int result = number1 + number2;
+            return result;
+        }
+
+        public int Subtraction(int number1, int number2)
+        {
+            int result = number1 - number2;
+            return result;
+        }
+    }
+}
