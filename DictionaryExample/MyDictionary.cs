edda400f0d2a4ee1f990cfeab823d1f6661e3993
Muhammet Bas <190201113@uzem.education>
2022-02-17 11:17:45 +0300

    8 Şubat

---
+++ DictionaryExample/MyDictionary.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryExample
+{
+    internal class MyDictionary<nT,aT>
+    {
+
+        nT[] names;
+        aT[] ages;
+        nT[] tempNames;
+        aT[] tempAges;
+
+        public MyDictionary()
+        {
+            names = new nT[0];
+            ages = new aT[0];
+        }
+
+        public int Count()
+        {
+            return names.Length;
+        }
+
+        public void Add(nT name, aT age)
+        {
+            tempNames = names;
+
+            names = new nT[names.Length + 1];
+
+            for (int i = 0; i < tempNames.Length; i++)
+            {
+                names[i] = tempNames[i];
+            }
+            names[names.Length - 1] = name;
+
+            tempAges = ages;
+
+            ages = new aT[ages.Length + 1];
+
+            for (int j = 0; j < tempAges.Length; j++)
+            {
+                ages[j] = tempAges[j];
+            }
+
+            ages[ages.Length - 1] = age;
+
+            Console.WriteLine(name + " " + age);
+        }
+    }
+}
---
+++ DictionaryExample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DictionaryExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> group = new MyDictionary<string, int>();
+            group.Add("Anıl", 12);
+            group.Add("Derya", 16);
+            group.Add("Burak", 20);
+            group.Add("Ercan", 18);
+            Console.WriteLine(group.Count());
+        }
+    }
+}
