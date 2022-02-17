b1dd2dfece27fb46b81f8b19bd4d29d8fc67ecaf
Muhammet Bas <190201113@uzem.education>
2022-02-17 10:43:37 +0300

    17 Şubat

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsIntro
+{
+    internal class MyList<T>
+    {
+        T[] items;
+        T[] tempArray;
+
+        public MyList()
+        {
+            items = new T[0];
+        }
+
+        public int Count
+        {
+            get { return items.Length; }
+        }
+
+        public void Add(T item)
+        {
+            tempArray = items;
+            items = new T[items.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+            items[items.Length - 1] = item;
+        }
+
+    }
+}
---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,20 @@
+﻿using GenericsIntro;
+using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> names = new MyList<string>();
+            Console.WriteLine(names.Count);
+
+            names.Add("Deniz");
+            names.Add("Derya");
+            names.Add("Kaptan");
+
+            Console.WriteLine(names.Count);
+        }
+    }
+}
