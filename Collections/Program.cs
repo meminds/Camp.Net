6e6898d525cf34657e4f3c033167fb2d0ff695a7
Muhammet Bas <190201113@uzem.education>
2022-02-17 10:28:36 +0300

    16 Şubat

---
+++ Collections/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string> { "Ali", "Yavuz", "Berk", "Naz" };
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+            names.Add("Emin");
+            Console.WriteLine(names[4]);
+            Console.WriteLine(names[0]);
+        }
+    }
+}
