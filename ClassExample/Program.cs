02308519c314c0a84adc9d48d50ed1dc1770c6a2
Muhammet Bas <190201113@uzem.education>
2022-02-17 12:01:31 +0300

    10  Şubat

---
+++ ClassExample/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ClassExample
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.ProductName = "Skirt";
+            product1.Price = 149.99;
+            product1.Stock = 10;
+
+            Product product2 = new Product();
+            product2.Id = 2;
+            product2.ProductName = "Shoes";
+            product2.Price = 450.99;
+            product2.Stock = 5;
+
+            Product product3 = new Product();
+            product3.Id = 3;
+            product3.ProductName = "T-shirt";
+            product3.Price = 99.99;
+            product3.Stock = 7;
+
+            Product[] products = new Product[] { product1, product2, product3 };
+
+            int i = 0;
+            while (i < products.Length)
+            {
+                Console.WriteLine(products[i].Id + " - " + products[i].ProductName + " : " + products[i].Price);
+                i++;
+            }
+            Console.WriteLine("\n--------While loop ended----------\n");
+
+            for (int j = 0; j < products.Length; j++)
+            {
+                Console.WriteLine(products[j].Id + " - " + products[j].ProductName + " : " + products[j].Price);
+            }
+            Console.WriteLine("\n----------For loop ended--------\n");
+
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product.Id + " - " + product.ProductName + " : " + product.Price);
+            }
+            Console.WriteLine("\n----------Foreach loop ended--------\n");
+        }
+    }
+}
+
+class Product 
+{
+    public int Id { get; set; }
+    public string ProductName { get; set; }
+    public double Price { get; set; }
+    public int Stock { get; set; }
+}
---
+++ ClassIntro/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+namespace ClassIntro
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Course course1 = new Course();
+			course1.CourseName = "C#";
+			course1.Instructor = "Engin Demiroğ";
+			course1.ViewsRate = 100;
+
+			Course course2 = new Course();
+			course2.CourseName = "Go";
+			course2.Instructor = "Derya Coluk";
+			course2.ViewsRate = 12;
+
+			Course course3 = new Course();
+			course3.CourseName = "Python";
+			course3.Instructor = "Mustafa Veryak";
+			course3.ViewsRate = 78;
+
+			Course[] courses = new Course[] { course1, course2, course3 };
+			foreach (Course course in courses)
+			{
+				Console.WriteLine(course.CourseName + ": " + course.Instructor);
+			}
+		}
+	}
+
+	class Course
+	{
+		public string CourseName { get; set; }
+		public string Instructor { get; set; }
+		public int ViewsRate { get; set; }
+	}
+
+
+}
