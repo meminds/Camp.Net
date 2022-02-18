a8ee50a28ad80190918c3cf5e6de6ccdbeca79f8
Muhammet Bas <190201113@uzem.education>
2022-02-18 20:44:18 +0300

    17 February

---
+++ Interface/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace Interface
+{
+    class Program
+    {
+        static void Main()
+        {
+            IWorker[] workers = new IWorker[3]
+            {
+                new Manager(),
+                new Worker(),
+                new Robot()
+            };
+
+            foreach (IWorker worker in workers)
+            {
+                worker.Work();
+            }
+
+            IEat[] eats = new IEat[2]
+            {
+                new Worker(),
+                new Manager()
+            };
+
+            foreach (IEat eat in eats)
+            {
+                eat.Eat();
+            }
+
+
+        }
+    }
+
+    interface IWorker
+    {
+        void Work();
+    }
+
+    interface IEat
+    {
+        void Eat();
+    }
+
+    interface ISalary
+    {
+        void GetSalary();
+    }
+
+    class Manager : IWorker, IEat, ISalary
+    {
+        public void Eat()
+        {
+            Console.WriteLine("This guy eats.");
+        }
+
+        public void GetSalary()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Work()
+        {
+            Console.WriteLine("This guy works.");
+        }
+    }
+
+
+    class Worker : IWorker, IEat, ISalary
+    {
+        public void Eat()
+        {
+            Console.WriteLine("This guy eats.");
+        }
+
+        public void GetSalary()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Work()
+        {
+            Console.WriteLine("This guy works.");
+        }
+    }
+
+    class Robot : IWorker
+    {
+        public void Work()
+        {
+            Console.WriteLine("This guy works.");
+        }
+    }
+
+
+}
